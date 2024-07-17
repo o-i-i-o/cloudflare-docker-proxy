22d8072d6df29a51296036c8affe9e0f9e18bd63
惊蛰 <100140586+o-i-i-o@users.noreply.github.com>
2024-07-17 11:06:30 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,13 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
-
-  // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker.solst.cn": "https://registry-1.docker.io",
+  "quay.solst.cn": "https://quay.io",
+  "gcr.solst.cn": "https://gcr.io",
+  "k8s-gcr.solst.cn": "https://k8s.gcr.io",
+  "k8s.solst.cn": "https://registry.k8s.io",
+  "ghcr.solst.cn": "https://ghcr.io",
+  "cloudsmith.solst.cn": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
